c6abb7d5125b237d5fabde7453c4061dd3c31f5b
Devaraj L <devarajldev01@gmail.com>
2025-09-09 16:33:00 +0530

    Update AdminOrders.jsx

--- Foodie/frontend/src/pages/AdminOrders.jsx
+++ Foodie/frontend/src/pages/AdminOrders.jsx
@@ -12,7 +12,7 @@ const AdminOrders = () => {
     setLoading(true);
     setError("");
     try {
-      const res = await fetch("http://localhost:6001/api/admin/orders", {
+      const res = await fetch("https://foodie-0b.onrender.com/api/admin/orders", {
         headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
       });
 
