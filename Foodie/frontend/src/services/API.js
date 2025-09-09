86696a32b9d9559d34749b98fde0a45aa1b46045
Devaraj L <devarajldev01@gmail.com>
2025-09-09 16:07:10 +0530

    Update API.js

--- Foodie/frontend/src/services/API.js
+++ Foodie/frontend/src/services/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://foodie-0.onrender.com/api",
+  baseURL: "https://foodie-0b.onrender.com//api",
 });
 
 export default API;
